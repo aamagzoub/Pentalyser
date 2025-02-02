93738aca2fb68bb5a4c98011afcf2c2474505723
aamagzoub <aboubaker.an@gmail.com>
2025-02-02 16:28:48 +0000

    Data manipulation
    - Extract data and display
    - Calculate average and display

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "gettersandsetters.h"
 
 #include <QString>
 #include <QDir>
@@ -8,7 +7,10 @@
 #include <QFile>
 #include <QFileInfo>
 #include <QFileDialog>
-
+#include <QMap>
+#include <QTableWidgetItem>
+#include <QDate>
+#include <QTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,97 +19,71 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     setWindowTitle("Pentalyser");
 
-    mpGettersAndSetters = new gettersAndSetters;
-
-    connect(ui->selectPathBtn,SIGNAL(clicked()),this,SLOT(browseFolders()));
-    connect(ui->useDefulatPathBtn,SIGNAL(clicked()),this,SLOT(useDefaultPath()));
-    connect(ui->closeAppBtn,SIGNAL(clicked()),this,SLOT(close()));
-    connect(ui->startExctractingBtn,SIGNAL(clicked()),this,SLOT(readCsvFiles()));
-    connect(ui->showAveragesBtn,SIGNAL(clicked()),this,SLOT(compareData()));
-    connect(ui->printBtn,SIGNAL(clicked()),this,SLOT(featureToBeIntroduced()));
-    connect(ui->saveAsCsvBtn,SIGNAL(clicked()),this,SLOT(featureToBeIntroduced()));
-    connect(ui->saveAsPdfBtn,SIGNAL(clicked()),this,SLOT(featureToBeIntroduced()));
-    connect(ui->emailBtn,SIGNAL(clicked()),this,SLOT(featureToBeIntroduced()));
-    connect(ui->tableWidget,SIGNAL(clicked(QModelIndex)),this,SLOT(countNumberOfRowsSelected()));
-    connect(ui->compareDataBtn,SIGNAL(clicked()),this,SLOT(compareSelectedRows()));
-    connect(ui->showAveragesBtn,SIGNAL(clicked()),this,SLOT(showAverages()));
-    connect(ui->clearRowsSelectionBtn,SIGNAL(clicked()),this,SLOT(clearRowsSelection()));
-
-    ui->tableWidget->setHorizontalHeaderLabels({
-        "Surname", "First Name", "Date", "Time", "Eye", "K1", "K2", "Pachy Min", "KMax"
-    });
-
+    connect(ui->selectPathBtn, &QPushButton::clicked, this, &MainWindow::browseFolders);
+    connect(ui->startExctractingBtn, &QPushButton::clicked, this, &MainWindow::startExtracting);
+    connect(ui->useDefaultPathBtn, &QPushButton::clicked, this, &MainWindow::useDefaultPath);
+    connect(ui->closeAppBtn, &QPushButton::clicked, this, &MainWindow::close);
 }
 
-void MainWindow::clearRowsSelection(){
-
-    ui->tableWidget->clearSelection();
-    ui->compareDataBtn->setEnabled(false);
-    ui->clearRowsSelectionBtn->setEnabled(false);
-
+void MainWindow::browseFolders() {
+    QString selectedPath = QFileDialog::getExistingDirectory(nullptr, "Select Folder", QDir::homePath());
+    if (selectedPath.isEmpty()) {
+        ui->statusTL->setText("WARNING: No path is selected! (default path will be used: " + getFilesPath() + ")");
+        return;
+    }
+    setFilesPath(selectedPath);
+    ui->statusTL->setText("Files Path: " + getFilesPath());
 }
 
-
-void MainWindow::showAverages(){
-    ui->tabWidget->setCurrentIndex(1);
-}
-
-
-void MainWindow::countNumberOfRowsSelected(){
-
-    ui->clearRowsSelectionBtn->setEnabled(true);
-
-    int numberOfRowsSelected = ui->tableWidget->selectionModel()->selectedRows().size();
-
-    if (numberOfRowsSelected == 3) {
-        ui->compareDataBtn->setEnabled(true);
-    } else {
-        ui->compareDataBtn->setEnabled(false);
+void MainWindow::useDefaultPath() {
+    if (getFilesPath() != defaultPath) {
+        setFilesPath(defaultPath);
+        ui->statusTL->setText("Files Path: " + getFilesPath());
     }
 }
 
-void MainWindow::compareSelectedRows(){
-    featureToBeIntroduced();
-}
-
-void MainWindow::useDefaultPath(){
+void MainWindow::startExtracting() {
     ui->tableWidget->clearContents();
-    mpGettersAndSetters->setInputFilesPath(defaultPath);
-    ui->startExctractingBtn->setEnabled(true);
-}
-
-void MainWindow::browseFolders(){
-
-    ui->tableWidget->clearContents();
-    QString selectedPath = QFileDialog::getExistingDirectory(nullptr, "Select Folder", QDir::homePath());
-    mpGettersAndSetters->setInputFilesPath(selectedPath);
-    ui->startExctractingBtn->setEnabled(true);
-}
-
-void MainWindow::readCsvFiles() {
     ui->tableWidget->setRowCount(0);
+    ui->tableWidget_2->clearContents();
+    ui->tableWidget_2->setRowCount(0);
 
-    QString selectedPath = mpGettersAndSetters->getInputFilesPath(); // Use the getter function to retrieve the path
+    ui->statusTL->setText("Files Path: " + getFilesPath());
 
-    QStringList csvFiles = getCsvFiles(selectedPath);
+    QStringList csvFiles = getCsvFiles(getFilesPath());
     if (csvFiles.isEmpty()) {
-        int currentRow = ui->tableWidget->rowCount();
-        ui->tableWidget->insertRow(currentRow);
-        ui->tableWidget->setItem(currentRow, 0, new QTableWidgetItem("No CSV files found"));
+        ui->statusTL->setText("WARNING: No CSV files found to process.");
         return;
     }
 
+    QMap<QString, QMap<QString, QList<double>>> averagesData;
+    QMap<QString, QStringList> nameData;
+    QMap<QString, int> fileCounts;
+
     for (const QString &fileName : csvFiles) {
         QString firstname, surname, date, time, eye;
-        QMap<QString, QString> extractedValues = parseCsvFile(selectedPath + "/" + fileName, firstname, surname, date, time, eye);
+        QMap<QString, QString> extractedValues = parseCsvFile(getFilesPath() + "/" + fileName, firstname, surname, date, time, eye);
 
         int currentRow = ui->tableWidget->rowCount();
         ui->tableWidget->insertRow(currentRow);
         populateTableRow(currentRow, firstname, surname, date, time, eye, extractedValues);
+
+        QString key = surname + ", " + firstname + " - " + date + " (" + eye + ")";
+        nameData[key] = {surname, firstname, date, eye};
+
+        fileCounts[key]++;
+
+        for (const QString &metric : extractedValues.keys()) {
+            averagesData[key][metric].append(extractedValues[metric].toDouble());
+        }
     }
 
-    // Resize columns to fit content after populating data
+    populateAveragesTable(averagesData, nameData, fileCounts);
+
     ui->tableWidget->resizeColumnsToContents();
+    ui->tableWidget->resizeRowsToContents();
+    ui->tableWidget_2->resizeColumnsToContents();
+    ui->tableWidget_2->resizeRowsToContents();
 }
 
 QStringList MainWindow::getCsvFiles(const QString &path) {
@@ -127,37 +103,91 @@ QMap<QString, QString> MainWindow::parseCsvFile(const QString &filePath, QString
     if (fileParts.size() >= 5) {
         surname = fileParts[0];
         firstname = fileParts[1];
-        eye = (fileParts[2].toLower() == "od") ? "D" :
-                  (fileParts[2].toLower() == "os") ? "S" :
-                  "Unknown";
+        eye = fileParts[2];
         date = formatDate(fileParts[3]);
         time = formatTime(fileParts[4].split('.').first());
-    } else {
-        firstname = "Unknown";
-        surname = "Unknown";
-        date = "Unknown";
-        time = "Unknown";
-        eye = "Unknown";
     }
 
     QTextStream in(&file);
     QStringList labels = {"Cornea Front Rh", "Cornea Front Rv", "Pachy Min", "K Max (Front)"};
-
     while (!in.atEnd()) {
-        QString line = in.readLine();
-        QStringList parts = line.split(';');
-
+        QStringList parts = in.readLine().split(';');
         if (parts.size() >= 2) {
             QString label = parts[0].trimmed();
             QString value = parts[1].trimmed();
             if (labels.contains(label)) {
-                extractedValues[label] = (label == "Cornea Front Rh" || label == "Cornea Front Rv") ? calculateFormula(value) : value;
+                extractedValues[label] = value;
             }
         }
     }
-
     file.close();
     return extractedValues;
+}
+
+
+
+void MainWindow::populateAveragesTable(const QMap<QString, QMap<QString, QList<double>>> &averagesData,
+                                       const QMap<QString, QStringList> &nameData,
+                                       const QMap<QString, int> &fileCounts) {
+    int row = 0;
+    for (auto it = averagesData.begin(); it != averagesData.end(); ++it) {
+        ui->tableWidget_2->insertRow(row);
+
+        QStringList nameFields = nameData[it.key()];
+        for (int i = 0; i < nameFields.size(); i++) {
+            QTableWidgetItem *item = new QTableWidgetItem(nameFields[i]);
+            item->setTextAlignment(Qt::AlignCenter);
+            ui->tableWidget_2->setItem(row, i, item);
+        }
+
+        int col = 4;
+        for (const QString &metric : {"Cornea Front Rh", "Cornea Front Rv", "Pachy Min", "K Max (Front)"}) {
+            double sum = 0;
+            for (double value : it.value()[metric]) {
+                sum += value;
+            }
+            double average = it.value()[metric].isEmpty() ? 0.0 : sum / it.value()[metric].size();
+
+            QString displayValue;
+            if (metric == "Cornea Front Rh" || metric == "Cornea Front Rv") {
+                displayValue = QString::number(337.5 / average, 'f', 1);
+            } else if (metric == "Pachy Min") {
+                displayValue = QString::number(qRound(average));
+            } else {
+                displayValue = QString::number(average, 'f', 2);
+            }
+
+            QTableWidgetItem *item = new QTableWidgetItem(displayValue);
+            item->setTextAlignment(Qt::AlignCenter);
+            ui->tableWidget_2->setItem(row, col++, item);
+        }
+
+        QTableWidgetItem *countItem = new QTableWidgetItem(QString::number(fileCounts[it.key()]));
+        countItem->setTextAlignment(Qt::AlignCenter);
+        ui->tableWidget_2->setItem(row, col, countItem);
+
+        row++;
+    }
+}
+
+void MainWindow::populateTableRow(int row, const QString &firstname, const QString &surname,
+                                  const QString &date, const QString &time, const QString &eye,
+                                  const QMap<QString, QString> &extractedValues) {
+    QStringList values = {surname, firstname, date, time, eye};
+    for (int i = 0; i < values.size(); i++) {
+        QTableWidgetItem *item = new QTableWidgetItem(values[i]);
+        item->setTextAlignment(Qt::AlignCenter);
+        ui->tableWidget->setItem(row, i, item);
+    }
+
+    QStringList labels = {"Cornea Front Rh", "Cornea Front Rv", "Pachy Min", "K Max (Front)"};
+    int col = 5;
+    for (const QString &label : labels) {
+        QString value = extractedValues.value(label, "N/A");
+        QTableWidgetItem *item = new QTableWidgetItem(value);
+        item->setTextAlignment(Qt::AlignCenter);
+        ui->tableWidget->setItem(row, col++, item);
+    }
 }
 
 QString MainWindow::formatDate(const QString &rawDate) {
@@ -170,100 +200,14 @@ QString MainWindow::formatTime(const QString &rawTime) {
     return parsedTime.isValid() ? parsedTime.toString("hh:mm:ss") : "Invalid Time";
 }
 
-QString MainWindow::calculateFormula(const QString &value) {
-    bool ok;
-    double numericValue = value.toDouble(&ok);
-    return (ok && numericValue != 0) ? QString::number(337.5 / numericValue, 'f', 2) : "Invalid";
+QString MainWindow::getFilesPath() {
+    return inputFilesPath.isEmpty() ? defaultPath : inputFilesPath;
 }
 
-void MainWindow::populateTableRow(int row, const QString &firstname, const QString &surname, const QString &date, const QString &time, const QString &eye, const QMap<QString, QString> &extractedValues) {
-    // Helper to create and configure table items
-    auto createTableItem = [](const QString &text, Qt::Alignment alignment = Qt::AlignLeft | Qt::AlignVCenter) {
-        QTableWidgetItem *item = new QTableWidgetItem(text);
-        item->setTextAlignment(alignment);
-        item->setFlags(item->flags() & ~Qt::ItemIsEditable); // Make item non-editable
-        return item;
-    };
-
-    // Create and configure table items for each column
-    ui->tableWidget->setItem(row, 0, createTableItem(surname)); // Surname
-    ui->tableWidget->setItem(row, 1, createTableItem(firstname)); // First Name
-    ui->tableWidget->setItem(row, 2, createTableItem(date, Qt::AlignCenter)); // Date
-    ui->tableWidget->setItem(row, 3, createTableItem(time, Qt::AlignCenter)); // Time
-    ui->tableWidget->setItem(row, 4, createTableItem(eye, Qt::AlignCenter)); // Eye
-    ui->tableWidget->setItem(row, 5, createTableItem(extractedValues.value("Cornea Front Rh", "Not found"), Qt::AlignCenter)); // K1
-    ui->tableWidget->setItem(row, 6, createTableItem(extractedValues.value("Cornea Front Rv", "Not found"), Qt::AlignCenter)); // K2
-    ui->tableWidget->setItem(row, 7, createTableItem(extractedValues.value("Pachy Min", "Not found"), Qt::AlignCenter)); // Pachy Min
-    ui->tableWidget->setItem(row, 8, createTableItem(extractedValues.value("K Max (Front)", "Not found"), Qt::AlignCenter)); // KMax
+void MainWindow::setFilesPath(const QString &path) {
+    inputFilesPath = path;
 }
 
-void MainWindow::setupTableWidget() {
-    // Set resize mode for all columns
-    for (int i = 0; i < ui->tableWidget->columnCount(); ++i) {
-        if (i == 0 || i == 1) {
-            // Surname and First Name columns dynamically resize
-            ui->tableWidget->horizontalHeader()->setSectionResizeMode(i, QHeaderView::Stretch);
-        } else {
-            // All other columns have fixed size
-            ui->tableWidget->horizontalHeader()->setSectionResizeMode(i, QHeaderView::Fixed);
-            ui->tableWidget->setColumnWidth(i, 120); // Fixed size for other columns
-        }
-    }
-
-    // Disable word wrapping globally
-    ui->tableWidget->setWordWrap(false);
-
-    // Enable horizontal scrolling for large tables
-    ui->tableWidget->setHorizontalScrollMode(QAbstractItemView::ScrollPerPixel);
-
-    // Update column headers
-    QStringList headers = {"Surname", "First Name", "Date", "Time", "Eye", "K1", "K2", "Pachy Min", "KMax"};
-    ui->tableWidget->setHorizontalHeaderLabels(headers);
-}
-
-
-void MainWindow::setUiButtons(){
-    ui->showAveragesBtn->setEnabled(true);
-    ui->startExctractingBtn->setEnabled(false);
-    ui->printBtn->setEnabled(true);
-    ui->saveAsCsvBtn->setEnabled(true);
-    ui->saveAsPdfBtn->setEnabled(true);
-    ui->emailBtn->setEnabled(true);
-    ui->showAveragesBtn->setEnabled(true);
-}
-
-void MainWindow::featureToBeIntroduced(){
-    qDebug() << "This feature is yet to be introdcued, apologies for the inconvenience ";
-}
-
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "gettersandsetters.h"
 #include <QMainWindow>
+#include <QString>
+#include <QMap>
+#include <QStringList>
+#include <QTableWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -15,34 +18,27 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    float formulaFactor = 337.5;
-
-    QString filesPath;
-    QString defaultPath = ("/Users/emagabu/workspace/Pentalyser/inputCsvFiles");
-
-    void setUiButtons();
-    void populateTableRow(int row, const QString &firstname, const QString &surname, const QString &date, const QString &time, const QString &eye, const QMap<QString, QString> &extractedValues);
+    QString getFilesPath();
+    void setFilesPath(const QString &path);
     QString formatDate(const QString &rawDate);
     QString formatTime(const QString &rawTime);
     QString calculateFormula(const QString &value);
     QStringList getCsvFiles(const QString &path);
     QMap<QString, QString> parseCsvFile(const QString &filePath, QString &firstname, QString &surname, QString &date, QString &time, QString &eye);
-    void setupTableWidget();
+    void populateTableRow(int row, const QString &firstname, const QString &surname, const QString &date, const QString &time, const QString &eye, const QMap<QString, QString> &extractedValues);
+    void populateAveragesTable(const QMap<QString, QMap<QString, QList<double> > > &averagesData, const QMap<QString, QStringList> &nameData, const QMap<QString, int> &fileCounts);
 private:
     Ui::MainWindow *ui;
-    gettersAndSetters *mpGettersAndSetters;
+    QString defaultPath = "/Users/emagabu/workspace/Pentalyser/inputCsvFiles";
+    QString inputFilesPath;
+
 private slots:
     void browseFolders();
     void useDefaultPath();
-    void featureToBeIntroduced();
-    void countNumberOfRowsSelected();
-    void compareSelectedRows();
-    void showAverages();
-    void clearRowsSelection();
-    void readCsvFiles();
+    void startExtracting();
 };
 
 #endif // MAINWINDOW_H
